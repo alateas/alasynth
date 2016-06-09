316e8cc2c2c899f62be0a54a82d6a959afc357b8
alateas <dmitrymashkin@gmail.com>
2016-06-09 17:26:33 +0400

    x

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
 from flask import Flask, render_template
-app = Flask(__name__, static_folder='static')
+app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World 2!'
-
-@app.route('/home')
 def home():
     """Render website's home page."""
     return render_template('home.html')
