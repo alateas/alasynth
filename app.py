413ce77c485f6d16f6876234a12685200f86abf4
alateas <dmitrymashkin@gmail.com>
2016-06-17 17:27:09 +0400

    first expirements

--- app.py
+++ app.py
@@ -9,5 +9,9 @@ def home():
 def synth():
     return render_template('synth.html')
 
+@app.route('/updown')
+def updown():
+    return render_template('updown.html')
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
